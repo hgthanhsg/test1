6cb30ffad3a1ef6ddb079293066e74fcc8b93475
hgthanhsg <hgthanhsg@gmail.com>
2022-08-06 22:18:59 +0700

    addnew

--- test1.py
+++ test1.py
@@ -2,6 +2,6 @@ This is what I am learning
 https://www.w3schools.com/python/python_comments.asp
 """
 if 5 > 2:
-    print ("adsfadfsa")
+    print ("adsfadfsa newadd")
 else:
     print ("adasdafaa")
