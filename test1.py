ee89bdf3d0c8508a6b3e95e21850ebf0fe0d660e
hgthanhsg <110707842+hgthanhsg@users.noreply.github.com>
2022-08-06 22:34:39 +0700

    Update test1.py

--- test1.py
+++ test1.py
@@ -2,6 +2,6 @@ This is what I am learning
 https://www.w3schools.com/python/python_comments.asp
 """
 if 5 > 2:
-    print ("adsfadfsa newadd")
+    print ("adsfadfsa newadd new thing")
 else:
     print ("adasdafaa")
